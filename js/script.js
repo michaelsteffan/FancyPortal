534eaca418f2dfe85f5029c48c6950ba476ee13b
David Rodriguez <limborodriguez553@gmail.com>
2016-08-13 23:31:59 -0400

    The speed-up update!
    Basically on this update I did a lot of optimization to the code and now the website loads a lot faster, also I removed the loading screen.

--- js/script.js
+++ js/script.js
@@ -3,5 +3,4 @@ var onlineip = "mc.hypixel.net"
 var clicktocopy = "Click to copy";
 var copied = "IP COPIED";
 var error = "Error";
-
-$.getJSON('https://mcapi.ca/query/' + onlineip + '/players',function(status){if(status.status==true) {$('.players-online').html(status.players.online);} else {$('.players-online').html(error).addClass('offline');}}); $(document).ready(function(){$("#copy").mouseover(function(){$("#copy").html(clicktocopy);});$("#copy").mouseleave(function(){$("#copy").html(ip);});$("#copy").click(function(){$("#copy").html(copied);});});
+$.getJSON("https://mcapi.ca/query/"+onlineip+"/players",function(a){1==a.status?$(".players-online").html(a.players.online):$(".players-online").html(error).addClass("offline")}),$(document).ready(function(){$("#copy").mouseover(function(){$("#copy").html(clicktocopy)}),$("#copy").mouseleave(function(){$("#copy").html(ip)}),$("#copy").click(function(){$("#copy").html(copied)})});
